1a545fe2213cff42f1751dc11f5476a0768e652f
Jakub Lisowski <lisowski.jakub@gmail.com>
2024-02-20 10:28:01 +0100

    ADD: Robot Component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,13 @@ import { Hero } from '../Hero/Hero'
 import { OurServices } from '../OurServices/OurServices'
 import elements1 from '../OurServices/OurServicesItems'
 import { WorldMap } from '../WorldMap/WorldMap'
+import { Robot } from '../Robot/Robot'
 // import LogoCarousel from '../LogoCarousel/LogoCarousel'
 
 export const Home = () => (
     <>
         <Hero />
         <OurServices elements={elements1} />
+        <Robot />
         <WorldMap />
-
     </>)
---
+++ src/components/Robot/Robot.jsx
@@ -0,0 +1,16 @@
+// import HeroBg from '/hero_bg.png'
+import { Trans } from 'react-i18next'
+
+
+export const Robot = () => (
+    <section className="robot">
+        <div className="robot__wrapper ">
+            <div className="robot__content">
+                <h2 className="robot__title"><Trans i18nKey="robot.title">WE AUTOMATE THE WORLD </Trans></h2>
+                <p className="robot__description"><Trans i18nKey="robot.description"></Trans></p>
+            </div>
+        </div>
+        <div className="robot__bg" alt="sorter"></div>
+    </section>
+)
+
