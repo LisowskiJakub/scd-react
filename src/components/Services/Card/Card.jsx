ef0c011f07c4dcd56b74903368b848d30cbe50c9
Jakub Lisowski <lisowski.jakub@gmail.com>
2024-03-09 17:17:56 +0100

    ADD:Sevices Card

---
+++ src/components/Services/Card/Card.jsx
@@ -0,0 +1,24 @@
+import { Trans } from "react-i18next"
+import css from './Card.module.css'
+
+
+export const Card = ({ picture, position, langKey }) => (
+    <div className={css.Card}>
+
+        <h2 className={css.title}><Trans i18nKey={langKey + ".title"}></Trans></h2>
+
+        <div className={css.wrapper}>
+            {position == "right" ?
+                <>
+                    <div className={css.description}><Trans i18nKey={langKey + ".description"}></Trans></div>
+                    <img src={picture}></img>
+                </>
+                :
+                <>
+                    <img src={picture}></img>
+                    <div className={css.description}><Trans i18nKey={langKey + ".description"}></Trans></div>
+                </>}
+        </div>
+    </div>
+
+)
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -3,21 +3,27 @@ import { HeroServices } from "./HeroServices/HeroServices"
 import { Trans } from "react-i18next"
 import css from "./Services.module.css"
 import picture from "/robotarm.png"
-
-
-
+import { Card } from "./Card/Card"
+import electricImg from '/src/assets/img/electric.png'
+import controlImg from '/src/assets/img/hmi.png'
+import laptopImg from '/src/assets/img/laptop.png'
 export const Services = () => (
 
-    <>        <section className={css.section}>
-        <h2 className={css.title}>
-            <Trans i18nKey={"section.ourServices.title"}></Trans>
-        </h2>
-        <p className={css.description}>
-            <Trans i18nKey={"section.ourServices.description"}>
-            </Trans>
-        </p>
-    </section>
+    <>
+        <section className={css.section}>
+            <h2 className={css.title}>
+                <Trans i18nKey={"section.ourServices.title"}></Trans>
+            </h2>
+            <p className={css.description}>
+                <Trans i18nKey={"section.ourServices.description"}>
+                </Trans>
+            </p>
+        </section>
         <HeroServices transKey={"heroMenu.services"} picture={picture} />
-
+        <section className={css.cardsSection}>
+            <Card picture={electricImg} langKey="services.electrical" />
+            <Card picture={controlImg} position="right" langKey="services.control" />
+            <Card picture={laptopImg} langKey="services.support" />
+        </section>
     </>
 )
